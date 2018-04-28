ebf65b83d8b9be728941c06278e1b37389cbc549
Meli <melissa.zellhuber@gmail.com>
2018-04-27 22:24:25 -0500

    linked list solution

--- LeetCode/merge-two-sorted-lists.py
+++ LeetCode/merge-two-sorted-lists.py
@@ -20,9 +20,9 @@
 """Merge two sorted lists"""
 def mergeLists(list1, list2):
 	"""
-    :type list1: [int], list2: [int]
-    :rtype: [int]
-    """
+	:type list1: [int], list2: [int]
+	:rtype: [int]
+	"""
 	merged = []
 	total = len(list1)+len(list2)
 
@@ -40,9 +40,59 @@ def mergeLists(list1, list2):
 		else:
 			merged.append(list1[0])
 			del list1[0]
-			
+
 	return merged
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#Linked List version
+
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+		
+
+def mergeListsLL(l1, l2):
+	"""
+	:type l1: ListNode
+	:type l2: ListNode
+	:rtype: ListNode
+	"""
+	if l1 and l2:
+		if l1.val > l2.val:
+			l1, l2 = l2, l1
+		l1.next = mergeListsLL(l1.next, l2)
+
+	return l1 or l2
 
 #expected output 1-1-2-3-4-4
 print(mergeLists([1,2,4], [1,3,4]))
+
+#list 1
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(4)
+
+#list 2
+node4 = ListNode(1)
+node5 = ListNode(3)
+node6 = ListNode(4)
+
+
+node1.next = node2
+node2.next = node3
+
+node4.next = node5
+node5.next = node6
+
+#expected output 1-1-2-3-4-4
+
+node = mergeListsLL(node1, node4)
+while node:
+	print node.val
+	node = node.next
